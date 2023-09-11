8a50cf38dc2c6fb5adea00e5f647ae1ad127a13c
lucaschabiron <lucaschabiron@icloud.com>
2023-09-11 09:01:39 +0200

    feat(font): add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
 	plugins: []
 };
