308d4df1437980fbfc9a66b7a67098b72e217c87
lucaschabiron <lucaschabiron@icloud.com>
2023-09-12 14:52:19 +0200

    fix(bg): to fixed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,13 +4,13 @@
 </script>
 
 <div
-	class="absolute -z-10 bg-gradient-to-r dark:from-slate-950 dark:via-slate-800 via-75% dark:to-slate-700 h-screen w-screen"
+	class="fixed -z-10 bg-gradient-to-r dark:from-slate-950 dark:via-slate-800 via-75% dark:to-slate-700 h-screen w-screen"
 />
 <div
-	class="opacity-5 -z-10 w-screen h-screen absolute top-0 left-0 bg-[url('/noisebackground.png')] bg-repeat"
+	class="opacity-5 -z-10 w-screen h-screen fixed top-0 left-0 bg-[url('/noisebackground.png')] bg-repeat"
 />
 <div class="min-h-screen p-2 flex">
-	<div class="max-w-screen md:w-1/3 sm:w-1/2">
+	<div class="w-screen lg:w-1/3 sm:w-1/2">
 		<Header />
 		<slot />
 	</div>
