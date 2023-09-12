d07b97686a828580361d1a85a15a39f2ce588c8d
lucaschabiron <lucaschabiron@icloud.com>
2023-09-12 15:07:39 +0200

    fix(background): swap to cover

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <div
-	class="fixed -z-10 bg-gradient-to-r dark:from-slate-950 dark:via-slate-800 via-75% dark:to-slate-700 h-screen w-screen"
+	class="fixed -z-10 bg-cover bg-gradient-to-r dark:from-slate-950 dark:via-slate-800 via-75% dark:to-slate-700 h-screen w-screen"
 />
 <div
-	class="opacity-5 -z-10 w-screen h-screen fixed top-0 left-0 bg-[url('/noisebackground.png')] bg-repeat"
+	class="opacity-5 object-cover -z-10 w-screen h-screen fixed top-0 left-0 bg-[url('/noisebackground.png')] bg-repeat"
 />
 <div class="min-h-screen p-2 flex">
 	<div class="w-screen lg:w-1/3 sm:w-1/2">
