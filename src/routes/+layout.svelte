d0ddeee5b76d3b2099f77ece53e968ba91997f3d
lucaschabiron <lucaschabiron@icloud.com>
2023-09-13 12:35:02 +0200

    fix(bg): meta theme color and remove gradient

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,7 @@
 	import Header from '$lib/components/Header.svelte';
 </script>
 
-<div
-	class="fixed -z-10 bg-cover bg-gradient-to-r dark:from-slate-950 dark:via-slate-800 via-75% dark:to-slate-700 h-screen w-screen"
-/>
+<div class="fixed -z-10 bg-cover dark:bg-slate-800 h-screen w-screen" />
 <div
 	class="opacity-5 object-cover -z-10 w-screen h-screen fixed top-0 left-0 bg-[url('/noisebackground.png')] bg-repeat"
 />
