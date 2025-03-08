f46b731ce06e8638a9b71069c5eae985d89bda76
Lucas Chabiron <84028247+lucaschabiron@users.noreply.github.com>
2025-03-08 21:46:40 +0100

    feat: full bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Navbar } from "@/components/global/nav";
 import FixedLogo from "@/components/global/FixedLogo";
+import Head from "next/head";
 
 export const metadata = {
   title: "lucas chabiron",
@@ -14,6 +15,12 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" className="font-sans">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+      </Head>
       <body className="min-h-screen from-neutral-50 to-neutral-400 text-neutral-900 bg-gradient-to-br dark:from-neutral-800 dark:to-neutral-900 dark:text-neutral-100">
         <FixedLogo />
         <div className="flex justify-center container mx-auto p-4">
