694420ce078e9e34413fa421774be4f3f098000c
Lucas Chabiron <84028247+lucaschabiron@users.noreply.github.com>
2025-06-30 23:38:51 +0200

    fix: homepage links

--- src/components/global/LinkedText.tsx
+++ src/components/global/LinkedText.tsx
@@ -12,7 +12,13 @@ export default function LinkedText({ text, links = {} }: LinkedTextProps) {
       {parts.map((part, i) => {
         if (part.startsWith("@") && links[part]) {
           return (
-            <a key={i} href={links[part]} target="_blank" rel="noopener noreferrer">
+            <a
+              key={i}
+              href={links[part]}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline"
+            >
               {part}
             </a>
           );
